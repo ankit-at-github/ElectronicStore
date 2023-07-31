fd2f4e29945fd604149b981fbab399103c6ff665
ankit-at-github <ankit.kumar.pune@gmail.com>
2023-07-30 21:00:48 -0400

    Adding Comment

--- src/main/java/com/lcwd/electronic/store/controllers/HomeController.java
+++ src/main/java/com/lcwd/electronic/store/controllers/HomeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String testing() {
+        //We are able to send String from here because we have used @RestController Annotation, if we had used only
+        //@Controller annotation, we would have written @ResponseBody with each @RequestMapping to send String else
+        //create view ----> string_name.html
         return "Welcome to Electronic Store.";
     }
 }
