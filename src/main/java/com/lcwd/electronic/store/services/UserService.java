099c522f9cb858971f4e929b883ee9f4ddd2a641
ankit-at-github <ankit.kumar.pune@gmail.com>
2023-07-30 22:58:47 -0400

    Added User Repository and User Service

---
+++ src/main/java/com/lcwd/electronic/store/services/UserService.java
@@ -0,0 +1,25 @@
+package com.lcwd.electronic.store.services;
+
+import com.lcwd.electronic.store.entities.User;
+
+public interface UserService {
+
+    //create
+
+    User createUser(User user);
+
+    //update
+
+    //delete
+
+    //get all users
+
+    //get single user by id
+
+    //get single usr by email
+
+    //search user
+
+    //other user specific features
+
+}
