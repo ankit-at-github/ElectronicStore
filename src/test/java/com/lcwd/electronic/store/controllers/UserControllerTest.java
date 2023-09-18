31a1bf014bf31970f230d289c8f8093838132e54
ankit-at-github <ankit.kumar.pune@gmail.com>
2023-09-17 20:36:54 -0400

    GetAllUser API - UserController

--- src/test/java/com/lcwd/electronic/store/controllers/UserControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/controllers/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.lcwd.electronic.store.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronic.store.dtos.PageableResponse;
 import com.lcwd.electronic.store.dtos.UserDto;
 import com.lcwd.electronic.store.entities.Role;
 import com.lcwd.electronic.store.entities.User;
@@ -16,6 +17,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
 import java.util.Set;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -72,5 +75,42 @@ public class UserControllerTest {
             e.printStackTrace();
             return null;
         }
+    }
+    @Test
+    public void updateUserTest() throws Exception {
+        UserDto dto = mapper.map(user, UserDto.class);
+        Mockito.when(userService.updateUser(Mockito.any(), Mockito.anyString())).thenReturn(dto);
+        String userId = "123";
+        //users/{userId} + put request + json
+        mockMvc.perform(MockMvcRequestBuilders.put("/users/"+userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObjectToJsonString(user))
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists());
+    }
+    @Test
+    public void getAllUsersTest() throws Exception {
+        UserDto object1 = UserDto.builder().name("Durgesh").email("durgesh@gmail.com").password("durgesh").about("Testing").build();
+        UserDto object2 = UserDto.builder().name("Sumit").email("durgesh@gmail.com").password("durgesh").about("Testing").build();
+        UserDto object3 = UserDto.builder().name("Ankit").email("durgesh@gmail.com").password("durgesh").about("Testing").build();
+        UserDto object4 = UserDto.builder().name("Uttam").email("durgesh@gmail.com").password("durgesh").about("Testing").build();
+
+        PageableResponse<UserDto> pageableResponse = new PageableResponse<>();
+        pageableResponse.setContent(Arrays.asList(object1, object2, object3, object4
+        ));
+        pageableResponse.setLastPage(false);
+        pageableResponse.setPageSize(10);
+        pageableResponse.setPageNumber(100);
+        pageableResponse.setTotalElements(1000);
+
+        Mockito.when(userService.getAllUser(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyString())).thenReturn(pageableResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
